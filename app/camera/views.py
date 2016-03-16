2dcb4ab9864b2012513120772b6785c60370f9f0
Elgardo Echevarria <elgardoe@hotmail.com>
2016-03-15 22:03:58 -0400

    live feed update
    bug fix with live feed and minor updates.

--- app/camera/views.py
+++ app/camera/views.py
@@ -5,13 +5,17 @@ from .. import db
 from ..models import Cameras
 from .forms import EditCameraSettings
 import os
+import time
 
 if os.getenv('EYECAM_CONFIG') == 'production':
+    print("importing pi camera")
     from camera_pi import Camera
 else:
+    print('importing sim camera')
     from camera_sim import Camera
 
 def gen(camera):
+    
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
@@ -23,24 +27,29 @@ def livefeed(camNum):
     
     form = EditCameraSettings()
     
+    print('livefeed')
+    
     if form.validate_on_submit():
+        print('This is a submit for update to ' + camNum)
         mycamera = Cameras()
         mycamera.cameraName = camNum
         mycamera.resolution = form.resolution.data
         mycamera.vflip = form.vflip.data
         mycamera.hflip = form.hflip.data
-        db.session.add(mycamera)
+        
+        mycamera.updateCameraSettings()
         flash('Your camera settings has been updated.')
         return render_template('camera/livefeed.html', form=form, camNum=camNum)
-      
+    
+    print('getting camera info')  
     mycamera = Cameras.query.filter_by(cameraName=camNum).first()
     if mycamera is None:
         abort(404)
     else:
-        flash(mycamera.resolution)
-        form.resolution = mycamera.resolution
-        form.vflip = mycamera.vflip
-        form.hflip = mycamera.hflip        
+        print mycamera.resolution
+        form.resolution.data = mycamera.resolution
+        form.vflip.data = mycamera.vflip
+        form.hflip.data = mycamera.hflip        
         return render_template('camera/livefeed.html', form=form, camNum=camNum)
 
 @camera.route('/video_feed')
--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ else:
     print('environmental file not found!')
             
 from app import create_app, db
-from app.models import User, Cameras
+from app.models import *
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
